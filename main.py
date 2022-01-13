4f0c7e18fdd60b604b060b7a6f6c6df04ddd0a68
Ayush Sharma <mrayushs933@gmail.com>
2022-01-14 01:15:32 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,75 @@
+import praw
+import os
+import time
+from keep_alive import keep_alive
+
+# keep_alive()
+
+reddit = praw.Reddit(
+    client_id=os.getenv("client_id"),
+    client_secret=os.getenv("client_secret"),
+    username=os.getenv("username"),
+    password=os.getenv("password"),
+    user_agent="edit bot (by u/Sr-grafo-edits)",
+)
+reddit.validate_on_submit = True
+ini_reply = """
+|Comment|Edit|Link|
+|:-:|:-:|:-:|
+"""
+db = {}
+
+
+def normalize(s):
+    s = s.replace('\n', ' ')
+    s = s.replace('>', "")
+    s.replace('!', "")
+    s.replace('`', "")
+    s.replace("#", "")
+    s.replace('_', "")
+    return s
+
+
+for comment in reddit.redditor("SrGrafo").stream.comments():
+    if not comment.is_submitter or not comment.parent_id[1] == "1":
+        continue
+    if "edit" not in comment.body.lower():
+        continue
+    par_submission = comment.submission
+    par_comment_text = reddit.comment(id=comment.parent_id[3:]).body
+    par_comment_text = normalize(par_comment_text)
+    comment_text = normalize(comment.body)
+    if max(len(par_comment_text), len(comment_text)) > 550:
+        continue
+    if par_submission.id not in db.keys():
+        try:
+            db[par_submission.id] = par_submission.reply(ini_reply + '\n').id
+            print("New submission added to DB")
+        except Exception as e:
+            print("Failed to add submission to DB")
+            print(f"Exception={e}")
+            print(f"Submission ID={par_submission.id}")
+            time.sleep(600)
+            continue
+    comment_to_edit = reddit.comment(id=db[par_submission.id])
+    comment_link = """https://reddit.com""" + comment.permalink
+    LINK = f"[Link]({comment_link})"
+    s = f"{par_comment_text}|{comment_text}|{LINK}"
+    reply = comment_to_edit.body + '\n' + s
+    try:
+        comment_to_edit.edit(reply)
+        print("Edited Comment Succesfully")
+        time.sleep(30)
+    except Exception as e:
+        print(f"Failed to edit comment.")
+        print(f'Exception ={e}')
+        print(f"Edit Comment = {comment_link}")
+
+
+def main():
+    pass
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
