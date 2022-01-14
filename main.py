d616524019e81d75f8238c39acd5082a5712afe8
Ayush Sharma <mrayushs933@gmail.com>
2022-01-14 17:53:29 +0530

    Cleaned things a bit.

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ import os
 import time
 from keep_alive import keep_alive
 
-# keep_alive()
+# TODO: Improve comment validation:
+#      1. Implement subreddit blacklisting
+#      2. Regex for imgur detection
+#      3: Comment body length check
 
 reddit = praw.Reddit(
     client_id=os.getenv("client_id"),
@@ -30,46 +33,48 @@ def normalize(s):
     return s
 
 
-for comment in reddit.redditor("SrGrafo").stream.comments():
+def valid(comment):
     if not comment.is_submitter or not comment.parent_id[1] == "1":
-        continue
+        return False
     if "edit" not in comment.body.lower():
-        continue
-    par_submission = comment.submission
-    par_comment_text = reddit.comment(id=comment.parent_id[3:]).body
-    par_comment_text = normalize(par_comment_text)
-    comment_text = normalize(comment.body)
-    if max(len(par_comment_text), len(comment_text)) > 550:
-        continue
-    if par_submission.id not in db.keys():
-        try:
-            db[par_submission.id] = par_submission.reply(ini_reply + '\n').id
-            print("New submission added to DB")
-        except Exception as e:
-            print("Failed to add submission to DB")
-            print(f"Exception={e}")
-            print(f"Submission ID={par_submission.id}")
-            time.sleep(600)
-            continue
-    comment_to_edit = reddit.comment(id=db[par_submission.id])
-    comment_link = """https://reddit.com""" + comment.permalink
-    LINK = f"[Link]({comment_link})"
-    s = f"{par_comment_text}|{comment_text}|{LINK}"
-    reply = comment_to_edit.body + '\n' + s
-    try:
-        comment_to_edit.edit(reply)
-        print("Edited Comment Succesfully")
-        time.sleep(30)
-    except Exception as e:
-        print(f"Failed to edit comment.")
-        print(f'Exception ={e}')
-        print(f"Edit Comment = {comment_link}")
+        return False
 
 
 def main():
-    pass
+    for comment in reddit.redditor("SrGrafo").stream.comments(skip_existing=True):
+        if not valid(comment):
+            continue
+        par_submission = comment.submission
+        par_comment_text = reddit.comment(id=comment.parent_id[3:]).body
+        par_comment_text = normalize(par_comment_text)
+        comment_text = normalize(comment.body)
+        if max(len(par_comment_text), len(comment_text)) > 550:
+            continue
+        if par_submission.id not in db.keys():
+            try:
+                db[par_submission.id] = par_submission.reply(ini_reply + '\n').id
+                print("New submission added to DB")
+            except Exception as e:
+                print("Failed to add submission to DB")
+                print(f"Exception={e}")
+                print(f"Submission ID={par_submission.id}")
+                time.sleep(600)
+                continue
+        comment_to_edit = reddit.comment(id=db[par_submission.id])
+        comment_link = """https://reddit.com""" + comment.permalink
+        LINK = f"[Link]({comment_link})"
+        s = f"{par_comment_text}|{comment_text}|{LINK}"
+        reply = comment_to_edit.body + '\n' + s
+        try:
+            comment_to_edit.edit(reply)
+            print("Edited Comment Succesfully")
+            time.sleep(30)
+        except Exception as e:
+            print(f"Failed to edit comment.")
+            print(f'Exception ={e}')
+            print(f"Edit Comment = {comment_link}")
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    # keep_alive()
     main()
